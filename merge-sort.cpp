df3c7a20f5c906db685aaab2a165a7e502e46203
SushantShekhar10 <42318174+SushantShekhar10@users.noreply.github.com>
2018-10-19 14:36:35 +0530

    Add files via upload
    Certain lines have been changed for better presentation

--- merge-sort.cpp
+++ merge-sort.cpp
@@ -1,30 +1,51 @@
 # include "includeAll.h"
+
 using namespace std;
 
+
 //Merge sort algorithm has a complexity of O(nlogn)
-void merge (int array[], int beg, int mid, int end) {
+
+void merge (int array[], int beg, int mid, int end)
+ {
+
 	
 	int i = 0, j = 0, k = 0, L[100] = { 0 }, R[100] = { 0 };
-	int n1 = mid - beg + 1;
+
+		int n1 = mid - beg + 1;
+	
 	int n2 = end - mid;
+
 	
 	//Initial array is divided into 2 halves. Left array 'L' and right array 'R'.
+	
 	for (i = 0; i < n1; i++)
+		
         L[i] = array[beg + i];
+			
     for (j = 0; j < n2; j++)
+			
         R[j] = array[mid + 1 + j];
+		
 	
 	i = 0; j = 0; k = 0;
+
 	
 	/* Compare elements of L and R, now when sorting in ascending order, 
 	all elements on the left half must be lesser than all elements on the right half.
 	So, the smaller of the two, L[i] and R[i] is updated in the main array. 
 	*/
-	while (i < n1 && j < n2) {
+
+		while (i < n1 && j < n2)
+ {
+
 		if (L[i] <= R[j])
+
 			array[k++] = L[i++];
+
 		else
+
 			array[k++] = R[j++];
+
 	}
 	/* Since L and R are not incremented equally in the previous while loop,
 	there may be some elements remaining in either L or R. These are just appended directly to the main array.
@@ -59,20 +80,32 @@ int main () {
 	cin >> size;
 	
 	cout << endl << "Enter elements: " << endl;
-	for (int i = 0; i < size; ++i)
-		cin >> array[i];
+	
+		for(int i = 0; i < size; ++i)
 
-	cout << endl << "Array before sorting: " << endl;
+			cin >> array[i];
+
+
+			cout << endl << "Array before sorting:\n " << endl;
+
 	for (int i = 0; i < size; ++i)
+
 		cout << array[i];
+
 	cout << endl;
+
 	
 	sort (array, 0, size-1);
+
 	
-	cout << endl << "Array after sorting: " << endl;
+	cout << endl << "Array after sorting:\n " << endl;
+
 	for (int i = 0; i < size; ++i)
+
 		cout << array[i];
+
 	
 	return 0;
+
 	
 }
